54a500abb8e4098cad2a2b5bb89c3a3f109b98a1
exactor <kyzya96@gmail.com>
2020-06-07 22:27:01 +0300

    comments for client.py

--- pyrapid/client.py
+++ pyrapid/client.py
@@ -1,16 +1,25 @@
+import os
 import socket
 
 from cloudpickle import dumps, loads
 
-from pyrapid.test_funcs import *
-
 BUFF = 1024 * 10
 
-HOST = 'localhost'
+HOST = '192.168.1.181'
 PORT = 8888
 
 
+def factorial1(num):
+    """Функция вычисления факториала"""
+    ts = time.monotonic()
+    f = 1
+    for i in range(1, num + 1):
+        f *= i
+    return f'last_num: {f % 10} ,factorial_size: {f.__sizeof__()}, {time.monotonic() - ts}'
+
+
 def send_func(func, args, exec_type, commands_dict=None):
+    """Функция отправки, сериализации и получения результата"""
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     transfer_dict = {
         'func': func,
@@ -29,17 +38,56 @@ def send_func(func, args, exec_type, commands_dict=None):
 
 if __name__ == '__main__':
     import time
-    from concurrent.futures import ThreadPoolExecutor
+    from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
-    tpe = ThreadPoolExecutor()
+    RUN_TIMES = 100
+    tpe = ThreadPoolExecutor(max_workers=RUN_TIMES)
+    ppe = ProcessPoolExecutor(max_workers=os.cpu_count())
     start = time.monotonic()
 
+    # симуляция отправки от разных клиентов
     t = tpe.map(send_func,
 
-                (factorial1 for i in range(1)),
-                ((100000,) for i in range(1)),
-                ('PROCESS' for i in range(1)),
-            )
+                (factorial1 for i in range(RUN_TIMES)),
+                ((100000,) for i in range(RUN_TIMES)),
+                ('PROCESS' for i in range(RUN_TIMES)),
+                )
 
     tpe.shutdown(wait=True)
-    print(f'duration: {time.monotonic() - start}')
+
+    print(f'distributed duration: {time.monotonic() - start}')
+    start = time.monotonic()
+
+    # Последовательное исполнение
+    for i in range(RUN_TIMES):
+        factorial1(100000)
+    print(f'local sync duration: {time.monotonic() - start}')
+
+    start = time.monotonic()
+    # Распараллеливание через процессы (не работает из-за большого количество одновременных задач)
+    p = ppe.map(factorial1,
+                (100000,) * RUN_TIMES, chunksize=os.cpu_count()
+
+                )
+    # Рабочий вариант распараллеливания через процессы
+    lst = []
+    for i in range(RUN_TIMES):
+        lst.append(ppe.submit(factorial1, 100000))
+    ppe.shutdown(wait=True)
+    for res in lst:
+        print(res.result())
+
+    print(f'local multiprocess duration: {time.monotonic() - start}')
+
+    # Локальное исполнение в потоках
+    tpe = ThreadPoolExecutor(max_workers=os.cpu_count())
+    start = time.monotonic()
+    p = tpe.map(factorial1,
+                (100000,) * RUN_TIMES
+
+                )
+    tpe.shutdown(wait=False)
+    for res in p:
+        print(res)
+
+    print(f'local multithread duration: {time.monotonic() - start}')
